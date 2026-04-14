28fbed249761af72d9de9dd7b13596e1d76c8332
falcon <falcon@menloinnovations.com>
2026-04-14 16:05:55 -0400

    change to invoke validation pipeline

--- force-app/main/default/lwc/test_component/test_component.js
+++ force-app/main/default/lwc/test_component/test_component.js
@@ -2,6 +2,6 @@ import { LightningElement } from "lwc";
 
 export default class Test_component extends LightningElement {
   testMethod = () => {
-    const vari = "test";
+    const vari = "new test";
   };
 }
